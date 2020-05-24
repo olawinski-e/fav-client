09ff71d5f0968130772e56aacfa967a31f84d8cf
Estelle Mac <estelleolawinski@MacBook-Pro-de-Estelle-2.local>
2020-05-24 17:35:20 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { HttpClientModule } from "@angular/common/http";
+import { LoginComponent } from "./login/login.component";
+import { AdminComponent } from "./admin/admin.component";
+import { HomeComponent } from "./home/home.component";
+import { LogoutComponent } from "./logout/logout.component";
+import { AuthGuard } from "./auth.guard";
+import { AuthService } from "src/services/auth.service";
+import { UserService } from "src/services/user.service";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    HomeComponent,
+    LogoutComponent,
+    DashboardComponent,
+    RegisterComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [AuthService, UserService, AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
